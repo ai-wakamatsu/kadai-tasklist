aad332c26991c0c005ccd8e57a128307e41bee6f
ai.wakamatsu <aibon-0427@hotmail.co.jp>
2020-06-24 07:39:41 +0000

    retake

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -34,7 +34,7 @@ class TasksController < ApplicationController
         flash[:success] = 'タスクが編集されました'
         redirect_to @task
     else
-        flash.now[:danger] = 'タスクが編集されませんでした'
+        flash.now[:danger] = '空欄では投稿できません'
         render :new
     end
   end
